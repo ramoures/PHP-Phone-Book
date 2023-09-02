4917819520e22415853ab8e7d3d1e86c925134e2
ramoures <ramoures@gmail.com>
2023-09-03 02:34:59 +0330

    update config

--- config.php
+++ config.php
@@ -39,6 +39,7 @@
         define('UPLOAD_PATH',ROOT_PATH.'media'.DS);
         define('AVATARS_DIR_NAME',"avatars");
         define('IMAGES_DIR_NAME','images');
+
     // Password validation
         define('PASSWORD_PATTERN','(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){2,})(?=(.*[!@#$%^&*_=+\-]){2,}).{8,16}'); 
         # 8-16 min-max character, [a-z]=(1 min char.), [A-Z]=(1 min char.), [0-9]=(2 min number.), [!@#$%^&*_=+\-]=(2 min char.)
@@ -49,7 +50,8 @@
     // Upload
         define('ALLOW_FILES_TYPE', array('image/jpg'=>'jpg','image/jpeg'=>'jpeg','image/png'=>'png'));
                                     # Mime types list: https://www.iana.org/assignments/media-types/media-types.xhtml
-        define('MAX_FILE_SIZE','3000000'); # 3 Megabytes = 3000000 Byte
+        define('MAX_FILE_SIZE','3000000'); # 3 Megabytes = 3,000,000 Byte
+
     // Date and Time
         define('TIMEZONE_TO_DISPLAY','UTC'); # Set ASIA/TEHRAN for Tehran timezone
                                              # List of Supported Timezones : https://www.php.net/manual/en/timezones.php
@@ -57,10 +59,12 @@
         define('TIME_FORMAT_TO_DISPLAY',"H:i:s");
         
         define('JALALI_CALENDAR',false); # true or false
+
     // Security
         define("DEBUG",false); # true or false
         #important: please change this value for your project:
         define('SECRET_KEY','D96B2B6A02850F2BF085DC39632E6A'); 
+        
     // Backend Pages Setting
         define("B_DEFAULT_LANG",'en'); # cookie set before
         define("B_LIMIT",'10'); # Limit post per pages
