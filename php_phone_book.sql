a13e8441e22b58eefc812dc93ebf1dab1abafa96
ramin <52276908+ramoures@users.noreply.github.com>
2024-06-16 20:05:56 +0330

    Added sql file for manual installation

---
+++ php_phone_book.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 16, 2024 at 05:42 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php_phone_book`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phbk_admins`
+--
+
+CREATE TABLE `phbk_admins` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `avatar_id` int(11) DEFAULT NULL,
+  `last_signed_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `phbk_admins`
+--
+
+--password is 123----
+INSERT INTO `phbk_admins` (`id`, `username`, `password`, `avatar_id`, `last_signed_at`, `updated_at`, `created_at`) VALUES
+(1, 'admin', 'e37331f827cb57a62815f225acee86724dc7e38733f660a8915cbcac8ea37d8c', NULL, NULL, NULL, '0000-00-00 00:00:00');
+--password is 123----
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phbk_phone_numbers`
+--
+
+CREATE TABLE `phbk_phone_numbers` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `nickname` varchar(100) NOT NULL,
+  `full_name` varchar(255) DEFAULT NULL,
+  `phone_numbers` text NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `image_id` int(11) DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phbk_upload`
+--
+
+CREATE TABLE `phbk_upload` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `folder` varchar(100) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `alt` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `phbk_admins`
+--
+ALTER TABLE `phbk_admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `unique_usersname` (`username`) USING BTREE;
+
+--
+-- Indexes for table `phbk_phone_numbers`
+--
+ALTER TABLE `phbk_phone_numbers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_nickname` (`nickname`),
+  ADD UNIQUE KEY `unique_phone_numbers` (`phone_numbers`) USING HASH;
+
+--
+-- Indexes for table `phbk_upload`
+--
+ALTER TABLE `phbk_upload`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `phbk_admins`
+--
+ALTER TABLE `phbk_admins`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `phbk_phone_numbers`
+--
+ALTER TABLE `phbk_phone_numbers`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `phbk_upload`
+--
+ALTER TABLE `phbk_upload`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
